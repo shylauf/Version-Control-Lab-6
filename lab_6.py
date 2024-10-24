0a47f3f8452361d41a8f28c5aeed7231b02eafc5
DhaniGirdhar <dhanigirdhar@ufl.edu>
2024-10-23 21:25:39 -0400

    decoder function + option 2

--- lab_6.py
+++ lab_6.py
@@ -13,6 +13,11 @@ def encode(password):
         encoded_pass += str(digit)
     return encoded_pass
 
+def decode(encoded_pass):
+	decoded = ""
+	for digit in encoded_pass:
+		decoded += str((int(digit) - 3) % 10)
+	return decoded
 
 if __name__ == '__main__':
     option = -1
@@ -26,16 +31,20 @@ if __name__ == '__main__':
         print('3. Quit')
         print()
         option = int(input('Please enter an option: '))
+
         # Reads password and encodes.
         if option == 1:
             password = input('Please enter your password to encode: ')
             password = encode(password)
             print('Your password has been encoded and stored!')
             print()
+
         # Decodes and prints both passwords (encoded & decoded).
         elif option == 2:
-            # IMPLEMENT
-            print()
+		decoded = decode(password)
+		print(f"The encoded password is {password}, and the original password is {decoded}")
+		print()
+
         # Exits the program
         elif option == 3:
             break
